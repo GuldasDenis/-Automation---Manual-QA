7b9410dac8ee5eaa53f18e57d4275b0f43f70de5
Guldas Denis <dennisodessa@gmail.com>
2023-02-08 18:35:20 +0200

    Add 4.4

--- task_4.py
+++ task_4.py
@@ -1,11 +1,15 @@
-# 4.1
-nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
-print(nat.replace("Fast", "Gigabit"))
-#4.2
-mac = "AAAA:BBBB:CCCC"
-print(mac.replace(":", "."))
-#4.3
-config = "switchport trunk allowed vlan 1,3,10,20,30,100"
-result = config.split()[4]
-result = result.split(',')
-print(result)
+# # 4.1
+# nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+# print(nat.replace("Fast", "Gigabit"))
+# #4.2
+# mac = "AAAA:BBBB:CCCC"
+# print(mac.replace(":", "."))
+# #4.3
+# config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+# result = config.split()[4]
+# result = result.split(',')
+# print(result)
+#4.4
+vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+vlans = set(vlans)
+print(sorted(vlans))
