2793e94460e9c77fff9d5e4394a4820fbb962bef
Guldas Denis <dennisodessa@gmail.com>
2023-02-08 19:19:00 +0200

    Add 4.5

--- task_4.py
+++ task_4.py
@@ -10,6 +10,15 @@
 # result = result.split(',')
 # print(result)
 #4.4
-vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
-vlans = set(vlans)
-print(sorted(vlans))
+# vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+# vlans = set(vlans)
+# print(sorted(vlans))
+#4.5
+command1 = "switchport trunk allowed vlan 1,2,3,5,8"
+command2 = "switchport trunk allowed vlan 1,3,8,9"
+result1 = command1.split()[4]
+result1 = result1.split(',')
+result2 = command2.split()[4]
+result2 = result2.split(',')
+preobrazovanie = list(set(result1) & set(result2))
+print(preobrazovanie)
