1cf2ce701880420c3104e8c46f49f7d6b45ba572
Guldas Denis <dennisodessa@gmail.com>
2023-02-09 19:36:36 +0200

    Add new folder

---
+++ john_gender and marta_gender/task_4new.py
@@ -0,0 +1,12 @@
+john_salary = float('1.2345')
+marta_salary = float('1.5432')
+print(john_salary,  marta_salary)
+
+john_salary = int('12345')
+marta_salary = int('15432')
+print(john_salary,  marta_salary)
+
+
+john_salary = str('12345')
+marta_salary = str('15432')
+print(john_salary,  marta_salary)
