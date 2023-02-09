bfed19efcaea8f5b5b2e6f1ede183321d4c90574
Guldas Denis <dennisodessa@gmail.com>
2023-02-09 11:51:11 +0200

    Add 4.6

--- task_4.py
+++ task_4.py
@@ -14,11 +14,23 @@
 # vlans = set(vlans)
 # print(sorted(vlans))
 #4.5
-command1 = "switchport trunk allowed vlan 1,2,3,5,8"
-command2 = "switchport trunk allowed vlan 1,3,8,9"
-result1 = command1.split()[4]
-result1 = result1.split(',')
-result2 = command2.split()[4]
-result2 = result2.split(',')
-preobrazovanie = list(set(result1) & set(result2))
-print(preobrazovanie)
+# command1 = "switchport trunk allowed vlan 1,2,3,5,8"
+# command2 = "switchport trunk allowed vlan 1,3,8,9"
+# result1 = command1.split()[4]
+# result1 = result1.split(',')
+# result2 = command2.split()[4]
+# result2 = result2.split(',')
+# preobrazovanie = list(set(result1) & set(result2)) # преобразовал список во множество(set), (&) операция пересецения множест (list) создал обратно список
+# print(preobrazovanie)
+#4.6
+ospf_route = "       10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+list1 = ospf_route.replace(",", "").replace("via", "").split() # Метод split() разбивает строку на части, используя как разделитель какой-то символ (или символы) и возвращает список строк:
+list2 = ["Prefix", "AD/Metric", "Next-Hop", "Last update", "Outbound Interface"]
+for list1, list2, list3 in zip(list1, list2): #Функция zip() принимает на входе несколько итерируемых объектов (iterable) или итераторов (iterators) и поэлементно группирует в кортежи. Функция останавливается, когда заканчиваются элементы в одном из источников. Относится к неперезапускаемым генераторам.
+    print('{:20}{:20}'.format(list2, list1))
+
+# #4.7
+# a = input("mac:   ")
+# mac = (a)
+# r = int(a.replace(":",""))
+# print(bin(r))
