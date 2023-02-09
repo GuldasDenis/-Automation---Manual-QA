4cda65caa22bd799f12ae537dfe58f5dd51e7975
Guldas Denis <dennisodessa@gmail.com>
2023-02-09 12:24:16 +0200

    Add 4.8

--- task_4.py
+++ task_4.py
@@ -23,14 +23,19 @@
 # preobrazovanie = list(set(result1) & set(result2)) # преобразовал список во множество(set), (&) операция пересецения множест (list) создал обратно список
 # print(preobrazovanie)
 #4.6
-ospf_route = "       10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
-list1 = ospf_route.replace(",", "").replace("via", "").split() # Метод split() разбивает строку на части, используя как разделитель какой-то символ (или символы) и возвращает список строк:
-list2 = ["Prefix", "AD/Metric", "Next-Hop", "Last update", "Outbound Interface"]
-for list1, list2, list3 in zip(list1, list2): #Функция zip() принимает на входе несколько итерируемых объектов (iterable) или итераторов (iterators) и поэлементно группирует в кортежи. Функция останавливается, когда заканчиваются элементы в одном из источников. Относится к неперезапускаемым генераторам.
-    print('{:20}{:20}'.format(list2, list1))
+# ospf_route = "       10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+# list1 = ospf_route.replace(",", "").replace("via", "").split() # Метод split() разбивает строку на части, используя как разделитель какой-то символ (или символы) и возвращает список строк:
+# list2 = ["Prefix", "AD/Metric", "Next-Hop", "Last update", "Outbound Interface"]
+# for list1, list2 in zip(list1, list2): #Функция zip() принимает на входе несколько итерируемых объектов (iterable) или итераторов (iterators) и поэлементно группирует в кортежи. Функция останавливается, когда заканчиваются элементы в одном из источников. Относится к неперезапускаемым генераторам.
+#     print('{:20}{:20}'.format(list2, list1))
 
 # #4.7
-# a = input("mac:   ")
-# mac = (a)
-# r = int(a.replace(":",""))
-# print(bin(r))
+# mac = "AAAA:BBBB:CCCC"
+# macstr = mac.replace(':', '')
+# res = ''.join(format(i, '08b') for i in bytearray(macstr, encoding='utf-8'))
+# print(res)
+
+#4.8
+ip = "192.168.3.1"
+oct1, oct2, oct3, oct4 = ip.split('.')
+print(f'{int(oct1):08b} {int(oct2):08b} {int(oct3):08b} {int(oct4):08b}')
