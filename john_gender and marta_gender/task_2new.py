72984970554cc3fd1421c4df5c89fb55cef30a76
Guldas Denis <dennisodessa@gmail.com>
2023-02-13 15:46:32 +0200

    New tasks

--- john_gender and marta_gender/task_2new.py
+++ john_gender and marta_gender/task_2new.py
@@ -1,17 +1,21 @@
 #1
 john_salary = float('1.2345')
 marta_salary = float('1.5432')
+
 print(john_salary,  marta_salary)
 #2
 john_salary = int('12345')
 marta_salary = int('15432')
+
 print(john_salary,  marta_salary)
 #3
 john_salary = str('12345')
 marta_salary = str('15432')
+
 print(john_salary,  marta_salary)
 #4
-john_salary = 5
-marta_salary = 3
-print(john_salary > marta_salary)
-print(john_salary < marta_salary)
+john_salary = []
+marta_salary = 5
+
+print(bool(john_salary), bool(marta_salary))
+
