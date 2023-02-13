2c27451e00de9bf9e624a3bcc601385de57541f9
Guldas Denis <dennisodessa@gmail.com>
2023-02-13 16:07:33 +0200

    update

--- john_gender and marta_gender/task_2new.py
+++ john_gender and marta_gender/task_2new.py
@@ -1,21 +1,41 @@
 #1
+
 john_salary = float('1.2345')
 marta_salary = float('1.5432')
 
 print(john_salary,  marta_salary)
+
 #2
+
 john_salary = int('12345')
 marta_salary = int('15432')
 
 print(john_salary,  marta_salary)
+
 #3
+
 john_salary = str('12345')
 marta_salary = str('15432')
 
 print(john_salary,  marta_salary)
+
 #4
+
 john_salary = []
 marta_salary = 5
 
 print(bool(john_salary), bool(marta_salary))
+
+#5
+
+john_friends = ['Ibragim', 'James', 'Peter']
+marta_friends = ['Ahmed', 'Muhamed', 'Rahmed']
+
+print(john_friends, marta_friends )
+
+#6
+
+peoples_names = ['Jon', 'Jon', 'Harry', 'Harry', 'Ahmed']
+
+print(set(peoples_names))
 
