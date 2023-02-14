4d8638b38b7312a86bb4a9680e2d420fdd26baaf
Guldas Denis <dennisodessa@gmail.com>
2023-02-14 20:43:38 +0200

    update

--- john_gender and marta_gender/task_2new.py
+++ john_gender and marta_gender/task_2new.py
@@ -1,47 +1,73 @@
-#1
-
-john_salary = float('1.2345')
-marta_salary = float('1.5432')
-
-print(john_salary,  marta_salary)
-
-#2
-
-john_salary = int('12345')
-marta_salary = int('15432')
-
-print(john_salary,  marta_salary)
-
-#3
-
-john_salary = str('12345')
-marta_salary = str('15432')
-
-print(john_salary,  marta_salary)
-
-#4
-
-john_salary = []
-marta_salary = 5
-
-print(bool(john_salary), bool(marta_salary))
-
-#5
-
+# #1
+#
+# john_salary = float('1.2345')
+# marta_salary = float('1.5432')
+#
+# print(john_salary,  marta_salary)
+#
+# #2
+#
+# john_salary = int('12345')
+# marta_salary = int('15432')
+#
+# print(john_salary,  marta_salary)
+#
+# #3
+#
+# john_salary = str('12345')
+# marta_salary = str('15432')
+#
+# print(john_salary,  marta_salary)
+#
+# #4
+#
+# john_salary = []
+# marta_salary = 5
+#
+# print(bool(john_salary), bool(marta_salary))
+#
+# #5
+#
 john_friends = ['Ibragim', 'James', 'Peter']
 marta_friends = ['Ahmed', 'Muhamed', 'Rahmed']
 
-print(john_friends, marta_friends )
+#print(john_friends, marta_friends )
 
 #6
 
 peoples_names = ['Jon', 'Jon', 'Harry', 'Harry', 'Ahmed']
 
-print(set(peoples_names))
+#print(set(peoples_names))
 
 #7
 
 tuple1 = ('43.219497', '27.961242')
 
-print(tuple1)
+#print(tuple1)
+#
+# #8
+#
+john = {
+    'full_name': 'John_McClane',
+    'Age': '32',
+    'Salary': '2000',
+    'Gender': True,
+    'Friends': john_friends,
+    'Coordinates': tuple1
+}
+
+marta = {
+    'full_name': 'Marta_Kostyuk',
+    'Age': '25',
+    'Salary': '3000',
+    'Gender': False,
+    'Friends': marta_friends,
+    'Coordinates': tuple1
+}
+
+print(john)
+print(marta)
+
+#8*
+
 
