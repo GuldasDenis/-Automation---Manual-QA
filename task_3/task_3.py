4f598b77e336fa09c1049f43fa8e51fa81ad9901
Guldas Denis <dennisodessa@gmail.com>
2023-02-15 09:40:28 +0200

    add task_3

---
+++ task_3/task_3.py
@@ -0,0 +1,14 @@
+#1
+string_with_names = "john peter brian Morgan Adam Maria bart"
+
+print(string_with_names.title())
+
+#2
+friends = ['John', 'Marta', 'Jeims', 'Amanda', 'Marinna']
+headname = 'Names'
+headname = headname.center(35, "*")
+
+print(headname)
+print('\n'.join(friends))
+
+
