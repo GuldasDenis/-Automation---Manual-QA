8ae9c1ad20f6aeff52494c594ec9876cc0ccb54f
Guldas Denis <dennisodessa@gmail.com>
2023-02-18 12:34:42 +0200

    home_task

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -4,11 +4,15 @@ string_with_names = "john peter brian Morgan Adam Maria bart"
 print(string_with_names.title())
 
 #2
-friends = ['John', 'Marta', 'Jeims', 'Amanda', 'Marinna']
-headname = 'Names'
-headname = headname.center(35, "*")
+friends = ["John", "Marta", "James", "Amanda", "Marianna"]
 
+headname = 'Names'
+headname = headname.center(20, "*")
 print(headname)
-print('\n'.join(friends))
+print(f"{friends[0]:>20}\n{friends[1]:>20}\n{friends[2]:>20}\n{friends[3]:>20}\n{friends[4]:>20}")
+
+#3
+
+
 
 
