33eb2aa5e0184ead390ba9373691ce737c1e2558
Guldas Denis <dennisodessa@gmail.com>
2023-02-19 17:38:45 +0200

    hw

--- task_4/task_4.py
+++ task_4/task_4.py
@@ -1,3 +1,17 @@
+#1
+my_array = [1, 2.5, "hello", True, [3, 4, 5]]
+
+print("For loop:")
+for i in my_array:
+    print(i)
+
+print("\nWhile loop:")
+i = 0
+while i < len(my_array):
+    print(my_array[i])
+    i += 1
+
+
 #2
 var = 10
 if var == 10:
@@ -6,3 +20,4 @@ else:
     result = False
 
 print(result)
+
