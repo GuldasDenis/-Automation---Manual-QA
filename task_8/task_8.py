0b173cc269ee88c3980b20a0aa6b7bbba9059058
Guldas Denis <dennisodessa@gmail.com>
2023-02-22 12:27:27 +0200

    task_8

--- task_8/task_8.py
+++ task_8/task_8.py
@@ -14,3 +14,15 @@ def arithmetic_operation(num1, num2, operation):
 print(arithmetic_operation(8, 2, '*'))
 print(arithmetic_operation(8, 2, '+'))
 print(arithmetic_operation(8, 2, '#'))
+
+#2
+
+side = 7
+
+def square(side):
+    perimeter = 4 * side
+    area = side ** 2
+    diagonal = (2 * side) ** 0.5
+    return (perimeter, area, diagonal)
+
+print(square(side))
